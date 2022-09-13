831da82a48ad81f6dfebe691a83f070db55bd0be
vitoremanuel00 <vitoremanuel@CGE747231.cge.mt.gov.br>
2022-09-13 18:17:45 -0400

    dequeue

--- LinkedList.c
+++ LinkedList.c
@@ -17,11 +17,12 @@ void init (LinkedList *list) {
     list -> size = 0;   
 }
 
+
 void enqueue(node *list, int num){ //inserindo no final da lista
     node *aux, *newnode = (node*)malloc(sizeof(node));
 
     if (newnode == NULL) return -1;{
-        newnode -> data = data ;
+        newnode->data=data;
         newnode -> next = NULL;
     }
 //é o primeiro?
@@ -31,17 +32,35 @@ void enqueue(node *list, int num){ //inserindo no final da lista
         }
         else{                               //se a lista nao estiver vazia, precisamos de uma repetiçao para percorrer ate o ultimo no, pra fazer ele apontar pro novo
             aux = *list;                    //ponteiro auxiliar para o primeiro no
-            while (aux -> next != NULL){    //usar o ponteiro pra percorrer a lista,
+            while (aux -> next != NULL)    //usar o ponteiro pra percorrer a lista,
                                             // enquanto existir um proximo ou nao for o ultimo a lista vai caminhar
                     aux = aux -> next;      //aux avança para o no seguinte
                     aux -> next = newnode;  //aux recebe o proximo que é nulo, e recebera o novo nó
             }
-        }
-    else{
-        printf("erro ao alocar memoria");
-    }
-    
+            list->size++; 
+            return 1;
 }
+    
+
+void* dequeue(LinkedList *List){
+    if(isEmpty == NULL) return NULL; //RETORNA QUE NADA FOI REMOVIDO POIS A LISTA ESTA VAZIA    
+
+    node *trash = list->first;       //variável que guarda o endereço do nó que será removido(no caso o primeiro nó da nossa lista)
+    list -> first = list->first->next;//o proximo apos o primeiro, passara a ser o primeiro da lista
+    //removendo dado do nó
+    void *data=trash->data;//dado do no removido
+    free(trash);
+    list->size--;
+    
+
+
+
+
+}
+
+
+
+
 int main(){
 
 
