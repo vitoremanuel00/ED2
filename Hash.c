ee786f141b46dfc96e2ca09f1b39d5f6c36861ed
vitoremanuel00 <vldesigner2021@gmail.com>
2022-09-29 17:10:56 -0300

    29-09

--- Hash.c
+++ Hash.c
@@ -2,3 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Hash.h"
+
+void initHash(HashStruct *hashStruct){
+    for (int i=0; i<MAX; i++){
+        init(&(hashStruct->hashes[i]));
+    }
+    hashStruct-> size = 0;
+}
+
+bool isHashEmpty(HashStruct *hashStruct){
+    return hashStruct->size==0;
+}
+
+int hash(char *key){
+    int peso=0;
+
+    for ((int) i = 0; key[i]!=0; i++)
+    peso+=key[i]*(i+1); //adcionamos peso a cada letra
+    
+    return peso%MAX; //para retornar o resto da divisao
+}
+
+int put(HashStruct *hashStruct, char *key, void *data, compare equal){
+    if (!containsKey(hashStruct,key, equal)) 
+    {
+        int res= enqueue(&HashStruct->hashes[(key)], data);
+        hashStruct->size+=res;
+    }
+    return 0;
+    
+}
+
